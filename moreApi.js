4f49c5f49b5a9e988b14f5708e8edf961fc5e8e4
its-MAHADi <mhkhandakar57@gmail.com>
2025-03-10 01:54:11 +0600

    moreApi practice

---
+++ moreApi.js
@@ -0,0 +1,13 @@
+const handleLoadUser = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        displayUser(data)
+    } )
+}
+
+
+const displayUser = (users) => {
+    console.log(users[2])
+}
